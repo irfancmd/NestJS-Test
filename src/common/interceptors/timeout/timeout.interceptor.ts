1683432d1d4747c524170bad221bd908802067c8
Chowdhury Mohammad Irfanuddin <mohammad.irfanuddin@technonext.com>
2024-10-30 02:14:04 +0600

    Fixed spelling error

--- src/common/interceptors/timeout/timeout.interceptor.ts
+++ src/common/interceptors/timeout/timeout.interceptor.ts
@@ -4,7 +4,7 @@ import { catchError, Observable, throwError, timeout, TimeoutError } from 'rxjs'
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // We will terminaate a request if we can't provide a response within 3 seconds.
+    // We will terminate a request if we can't provide a response within 3 seconds.
     // The timeout() operator of RxJS allows us to implement it quite simply like this.
     return next.handle().pipe(timeout(3000), catchError(err => {
       if(err instanceof TimeoutError) {
